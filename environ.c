45dd0ecf5762615e4f037c6eacf692a37f34fdf0
AntoineJacob <5646@holbertonstudents.com>
2022-12-06 05:31:21 -0800

    Adding first version of shell

---
+++ environ.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * print_env - function that prints all enviromental variables.
+ * @env: pointer to enviromental variables PATH.
+ */
+void print_env(char **env)
+{
+	size_t i = 0, len = 0;
+
+	while (env[i])
+	{
+		len = _strlen(env[i]);
+		write(STDOUT_FILENO, env[i], len);
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
+	}
+}
